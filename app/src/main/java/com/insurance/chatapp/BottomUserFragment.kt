a4f713b964f7fd0e408960e2c783f1571c51de11
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-04-20 18:06:32 +0400

    add message input

---
+++ app/src/main/java/com/insurance/chatapp/BottomUserFragment.kt
@@ -0,0 +1,25 @@
+package com.insurance.chatapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.insurance.chatapp.databinding.FragmentBottomUserBinding
+
+class BottomUserFragment : Fragment() {
+    private lateinit var binding: FragmentBottomUserBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBottomUserBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/insurance/chatapp/MainActivity.kt
+++ app/src/main/java/com/insurance/chatapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.insurance.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.insurance.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.top_user_fragment, TopUserFragment())
+            add(R.id.bottom_user_fragment, BottomUserFragment())
+        }.commit()
     }
 }
