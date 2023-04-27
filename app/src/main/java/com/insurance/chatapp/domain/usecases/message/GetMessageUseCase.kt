bfe410689a67c6f011d1e936caa67b596b40ee75
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-04-27 18:28:12 +0400

    update useCase

--- app/src/main/java/com/insurance/chatapp/domain/usecases/message/GetMessageUseCase.kt
+++ app/src/main/java/com/insurance/chatapp/domain/usecases/message/GetMessageUseCase.kt
@@ -1,6 +1,5 @@
 package com.insurance.chatapp.domain.usecases.message
 
-import com.insurance.chatapp.domain.models.message.MessageModel
 import com.insurance.chatapp.domain.repositorys.MessageRepository
 import javax.inject.Inject
 
